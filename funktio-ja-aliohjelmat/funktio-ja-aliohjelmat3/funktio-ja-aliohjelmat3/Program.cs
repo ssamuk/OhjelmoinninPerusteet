05e57841e203a58fd6e1b7d2c154f048a2d0558d
Samu Kaijansinkko <samukaijansinkko@gmail.com>
2019-10-28 14:33:54 +0200

    third function task almost ready

--- funktio-ja-aliohjelmat/funktio-ja-aliohjelmat3/funktio-ja-aliohjelmat3/Program.cs
+++ funktio-ja-aliohjelmat/funktio-ja-aliohjelmat3/funktio-ja-aliohjelmat3/Program.cs
@@ -6,7 +6,40 @@ namespace funktiojaaliohjelmat3
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            int retNumber = numberFromRange(1, 100);
+            Console.WriteLine($"Your input {retNumber} passes test!");
+
         }
+
+        static int numberFromRange(int lowerBound, int upperBound)
+        {
+
+            
+            int inputNumb = 0;
+            int ret = 0;
+
+
+            do
+            {
+
+                Console.Write("Give a random number between numbers 1 and 100 then press enter: ");
+                inputNumb = int.Parse(Console.ReadLine());
+
+                if (inputNumb >= lowerBound && inputNumb <= upperBound)
+                {
+                    return inputNumb;
+                }
+                else
+                {
+                    Console.WriteLine("Error, try again.");
+                    
+                }
+            } while (inputNumb < lowerBound || inputNumb > upperBound);
+            return inputNumb;
+        }
+        
+        
+       
     }
 }
