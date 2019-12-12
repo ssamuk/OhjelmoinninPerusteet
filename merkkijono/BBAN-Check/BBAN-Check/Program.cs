b441200bce1a4851efcb9d1dbe8f9de808fc74d0
Samu Kaijansinkko <samukaijansinkko@gmail.com>
2019-12-12 19:19:56 +0200

    String-Handling
    Somehow i got BBAN-validator to fill input to 14 numbers and program does it as bank rules say.

--- merkkijono/BBAN-Check/BBAN-Check/Program.cs
+++ merkkijono/BBAN-Check/BBAN-Check/Program.cs
@@ -9,37 +9,83 @@ namespace BBANCheck
             Console.Write("Insert BBAN: ");
             string userInput = Console.ReadLine();
             userInput = userInput.Replace("-", "");
-           // Int32.TryParse(userInput, out int j);
+            int i = 0;
             
-            WhichBank(ref userInput);
+            WhichBank(userInput, ref i);
+            machineNumber(ref userInput, i);
+            Console.WriteLine(userInput);
+           
             
-        }
 
-        static void WhichBank(ref string userInput)
+        }
+        //this method defines where we put zeros in 'userinput',
+        //so this is raw/lazy way to define the bank
+        static void WhichBank(string userInput, ref int i)
+        {
+            char firstNumb = userInput[0];
+            Console.WriteLine(firstNumb);
+
+            if (firstNumb == '4' || firstNumb == '5' || firstNumb == '7')
+            {
+                i = 1;
+            }
+            else
+                i = 2;
+        }
+        static void machineNumber(ref string userInput, int i)
         {
             
-            Console.WriteLine(userInput[0]);
-
-
-
-            Console.ReadKey();
-
-
-
+            if (i == 1)
+            {
+                while(userInput.Length < 14)
+                {
+                    userInput = userInput.Insert(7, "0");
+                }
+            }
+            else
+            {
+                while(userInput.Length < 14)
+                {
+                    userInput = userInput.Insert(6,"0");
+                }
+            }
         }
-
     }
 }
-          /*1 = Nordea Pankki(Nordea)
-            2 = Nordea Pankki(Nordea)
-            31 = Handelsbanken
-            33 = Skandinaviska Enskilda Banken(SEB)
-            34 = Danske Bank
-            36 = Tapiola Pankki
-            37 = DnB NOR Bank ASA(DnB NOR)
-            38 = Swedbank
-            39 = S - Pankki
-            4 = Aktia Pankki, Säästöpankit(Sp) ja Paikallisosuuspankit(POP)
-            5 = OP - Pohjola - ryhmä(Osuuspankit(OP), Helsingin OP Pankki ja Pohjola Pankki)
-            6 = Ålandsbanken ÅAB)
-            8 = Sampo Pankki*/
+
+
+
+
+/*
+ *
+ * 
+ *
+1 = Nordea Pankki(Nordea)
+2 = Nordea Pankki(Nordea)
+31 = Handelsbanken
+33 = Skandinaviska Enskilda Banken(SEB)
+34 = Danske Bank
+36 = Tapiola Pankki
+37 = DnB NOR Bank ASA(DnB NOR)
+38 = Swedbank
+39 = S - Pankki
+4 = Aktia Pankki, Säästöpankit(Sp) ja Paikallisosuuspankit(POP)
+5 = OP - Pohjola - ryhmä(Osuuspankit(OP), Helsingin OP Pankki ja Pohjola Pankki)
+6 = Ålandsbanken ÅAB)
+8 = Sampo Pankki
+*
+- IndexOf
+- Insert
+- Length
+- Replace
+- Remove
+- Split
+- Substring
+- Trim
+- ToUpper
+- ToLower
+*
+*
+*
+* 
+*/
