c95f8d1536fa7e5430a079679a81daf56816f0e1
samu <samukaijansinkko@gmail.com>
2019-12-18 22:10:42 +0200

    FileHandling
    Testing file handling on my home pc.

--- FileHandling/FileHandling/Program.cs
+++ FileHandling/FileHandling/Program.cs
@@ -6,24 +6,12 @@ class Test
     public static void Main()
     {
         Console.WriteLine("Tiedoston kirjoitus ja luku esimerkki.");
-        string path = @"Users\Samu\Desktop\MyTest.txt";
+        string path = @"MyTest.txt";
         // string path = "c\\temp\\MyTest.txt";                      <----   // Tämä on vanhempi tapa kun @ merkki ei ollut käytössä!
-        //if (!File.Exists(path))
-        //{
-        // Create a file to write to.
-        
-            
-       // }
 
-        // Open the file to read from.
-        using (StreamReader sr = File.OpenText(path))
-        {
-            string s;
-            while ((s = sr.ReadLine()) != null)
-            {
-                Console.WriteLine(s);
-            }
-        }
+        WriteToFile(path);
+        ReadFile(path);
+        Console.ReadKey();
     } // End main program
 
     static void WriteToFile(string path)
@@ -40,6 +28,16 @@ class Test
 
         }
     }
-
+    static void ReadFile(string path)
+    {
+        using (StreamReader sr = File.OpenText(path))
+        {
+            string s;
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
 
 }
